d881aef06858e0200e167d7c7273dd156c03bfc9
lauraparrado <laespaviza@gmail.com>
2021-10-24 12:44:00 -0500

    component navbar layout

---
+++ src/layouts/GeneralLayout.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <header>
+            <Navbar />
+        </header>
+    </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+export default {
+  name: 'GeneralLayout',
+  components: {
+    Navbar
+  }
+}
+</script>
---
+++ src/layouts/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+    <nav class="navbar">
+        <ul class="navbar__nav">
+            <a class="navbar__house" href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
+                <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z"/>
+                </svg>
+            </a>
+            <li class="navbar__item">
+                <a class="navbar__link" href="#">EMPRESAS</a>
+                <div class="line"></div>
+            </li>
+            <li class="navbar__item">
+                <a class="navbar__link" href="#">NOMBRE EMPRESA</a>
+                <div class="line"></div>
+            </li>
+        </ul>
+        <div class="navbar__profile">
+            <img src="@/assets/user.png">
+            
+            <div class="dropdown">
+                <button>Usuario Apellido</button>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-compact-down" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M1.553 6.776a.5.5 0 0 1 .67-.223L8 9.44l5.776-2.888a.5.5 0 1 1 .448.894l-6 3a.5.5 0 0 1-.448 0l-6-3a.5.5 0 0 1-.223-.67z"/>
+                </svg>
+                <div class="navbar__profile__content">
+                    <a href="#">Profile</a>
+                    <a href="#">Logout</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
