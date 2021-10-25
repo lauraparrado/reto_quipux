c31f9503707f2b8238505962e0b1cc21e903bddb
lauraparrado <laespaviza@gmail.com>
2021-10-25 03:51:15 -0500

    Sidebar completado

--- src/layouts/GeneralLayout.vue
+++ src/layouts/GeneralLayout.vue
@@ -4,7 +4,7 @@
             <Navbar />
         </header>
         <div>
-          <SideBar />
+          <Sidebar :items="sidebarItems" />
            <router-view></router-view>
         </div>
         <footer>
@@ -16,11 +16,42 @@
 <script>
 import Footer from './components/Footer.vue'
 import Navbar from './components/Navbar.vue'
+import Sidebar from './components/Sidebar.vue'
 export default {
   name: 'GeneralLayout',
+  data() {
+    return {
+      sidebarItems:[{
+  "title": "Empresas",
+  "subitems": [
+    {
+      "title": "Empresas",
+      "urlName": "/"
+    }
+  ]
+}, {
+  "title": "Artículos",
+  "subitems": [
+    {
+      "title": "Artículos",
+      "urlName": "/"
+    }
+  ]
+}, {
+  "title": "Colaboradores",
+  "subitems": [
+    {
+      "title": "Colaboradores",
+      "urlName": "/"
+    }
+  ]
+}]
+    }
+  },
   components: {
     Navbar,
     Footer,
+    Sidebar,
   }
 }
 </script>
